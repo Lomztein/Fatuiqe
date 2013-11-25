9348dbf1a623ee39f3214777463a4501377e314e
Lomztein <lomztein2@gmail.com>
2013-11-25 11:23:55 +0100

    Added a few things and added a buttom to quit
    Escape is used to quit the game.

--- Assets/Scripts/LightstickScript.cs
+++ Assets/Scripts/LightstickScript.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class LightstickScript : MonoBehaviour {
 	
-	public int time;
 	public float fadeRate;
 
 	// Use this for initialization
 	void Update () {
-		light.intensity -= fadeRate * Time.deltaTime;
+		light.intensity -= (fadeRate * Time.deltaTime);
 		if (light.intensity <= 0) {
 			Destroy (gameObject);
 		}
